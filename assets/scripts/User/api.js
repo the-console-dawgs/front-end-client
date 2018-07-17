7e509cf1e8d9f452ed6d77d5f22f0696792861fd
priyanka.harkare@gmail.com <priyanka.harkare@gmail.com>
2018-07-17 13:27:42 -0400

    Added the ajax for the user auth in api.js

---
+++ assets/scripts/User/api.js
@@ -0,0 +1,40 @@
+const store = require('../scripts/store')
+const config = require('../scripts/config')
+
+const register = function (data) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-up',
+    data: data
+  })
+}
+
+const logIn = function (data) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-in',
+    data: data
+  })
+}
+
+const changePassword = function (data) {
+  return $.ajax({
+    method: 'PATCH',
+    url: config.apiUrl + '/change-password',
+    data: data,
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const signOut = function (data) {
+  return $.ajax({
+    method: 'DELETE',
+    url: config.apiUrl + '/sign-out',
+    data: data,
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
